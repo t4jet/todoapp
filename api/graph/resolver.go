02aed3e01d29a1d8580ea6002a2a42b209e67e66
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-06-18 11:51:39 +0900

    endpoint

--- api/graph/resolver.go
+++ api/graph/resolver.go
@@ -3,5 +3,8 @@ package graph
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
+import "github.com/t4jet/gqlgen-tods/graph/model"
 
-type Resolver struct{}
+type Resolver struct {
+	todos []*model.Todo
+}
